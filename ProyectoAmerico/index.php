7006e521e93cec33490f18c1936877a9527d1980
Maritza Vega <2016200251@untels.edu.pe>
2021-12-06 19:59:18 -0500

    Cambios en index.php

--- ProyectoAmerico/index.php
+++ ProyectoAmerico/index.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema VeI Americo-Login</title>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/estilos.css">
     <link rel="stylesheet" href="css/style.css">
 
@@ -28,7 +27,6 @@
     <!-------------------RecuadroLogin---------------->
     <br>
     <div class="contenedor-form">
-        
         <div class="formLogin all-tittles">
             <img src="imagenes/logo.JPG">
             <form action="controlador/validarLogin.php">
