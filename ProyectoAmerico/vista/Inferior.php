b7f0dec964c4b69aed52b67693535f2f185fa2fc
Carla Lizarraga Vasquez <2016200129@untels.edu.pe>
2021-12-07 20:36:10 -0500

    Inferior

--- ProyectoAmerico/vista/Inferior.php
+++ ProyectoAmerico/vista/Inferior.php
@@ -6,11 +6,11 @@
                 
                         <h4 class="all-tittles">Acerca de</h4>
                         <p>
-                            Estudiantes de la Universidad Nacional Tecnológica de Lima Sur que han desarrollado un Sistema de Ventas e Inventario.
+                            Estudiantes de la Universidad Nacional Tecnológica de Lima Sur que han desarrollado un Sistema de Gestión de Ventas en la empresa Américo.
                         </p>
                     </div>
                     <div class="col-xs-12 col-sm-6">
-                        <h4 class="all-tittles">Desarrolladoras</h4>
+                        <h4 class="all-tittles">Participantes del proyecto</h4>
                         <ul class="list-unstyled">
                             <li><i class="zmdi zmdi-check zmdi-hc-fw"></i>&nbsp; Joselyn Anaya <a href="https://www.facebook.com/joselyn.anaya.1"><i class="zmdi zmdi-facebook zmdi-hc-fw footer-social"></a></i></i></li> 
                             <li><i class="zmdi zmdi-check zmdi-hc-fw"></i>&nbsp; Carla Lizárraga <a href="https://www.facebook.com/carla.lizarraga.7509"><i class="zmdi zmdi-facebook zmdi-hc-fw footer-social"></a></i></i></li>  
@@ -21,7 +21,7 @@
                     </div>
                 </div>
             </div>
-            <div class="footer-copyright">© 2021 Sistema de Ventas e Inventario</div>
+            <div class="footer-copyright">© 2021 Sistema de Gestión de Ventas Américo</div>
         </footer>
     </div>
 </body>
